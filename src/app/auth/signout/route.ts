a7ca252d95f0641d00105d38814e70d39171ba1e
Me-HimanshuJain <himanshu440alw@gmail.com>
2026-09-21 17:15:55 +0530

    Fix signout 405 error

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -4,5 +4,7 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const supabase = createClient();
   await supabase.auth.signOut();
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/", request.url), {
+    status: 303,
+  });
 }
